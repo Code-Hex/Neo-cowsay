2d6c6b80dd80b3899a70106c9444d5d201a45a85
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-07-13 19:02:33 +0900

    fixed to use embed

---
+++ embed.go
@@ -0,0 +1,27 @@
+package cowsay
+
+import (
+	"embed"
+	"path/filepath"
+)
+
+//go:embed cows/*
+var cowsDir embed.FS
+
+func Asset(path string) ([]byte, error) {
+	return cowsDir.ReadFile(path)
+}
+
+func AssetNames() []string {
+	const cows = "cows"
+	entries, err := cowsDir.ReadDir("cows")
+	if err != nil {
+		panic(err)
+	}
+	names := make([]string, 0, len(entries))
+	for _, entry := range entries {
+		filename := filepath.Join("cows", entry.Name())
+		names = append(names, filename)
+	}
+	return names
+}
