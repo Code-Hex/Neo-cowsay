782452ed5a21100577c2c5e2c0bd41ec0415a9c4
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-11-03 00:19:26 +0900

    use strings.Builder in Rainbow and Aurora

--- rainbow.go
+++ rainbow.go
@@ -18,43 +18,45 @@ var rainbow = []int{magenta, red, yellow, green, cyan, blue}
 
 // Rainbow to generate rainbow string
 func (cow *Cow) Rainbow(mow string) string {
+	defer cow.buf.Reset()
+
 	var attribute string
 	if cow.bold {
 		attribute = ";1"
 	}
 
-	buf := make([]rune, 0, len(mow))
 	i := 0
 	for _, char := range mow {
 		if char == '\n' {
 			i = 0
-			buf = append(buf, char)
+			cow.buf.WriteRune(char)
 			continue
 		}
-		buf = append(buf, []rune(fmt.Sprintf("\x1b[%d%sm%c\x1b[0m", rainbow[i%6], attribute, char))...)
+		fmt.Fprintf(&cow.buf, "\x1b[%d%sm%c\x1b[0m", rainbow[i%6], attribute, char)
 		i++
 	}
 
-	return string(buf)
+	return cow.buf.String()
 }
 
 // Aurora to generate gradation colors string
 func (cow *Cow) Aurora(i int, mow string) string {
+	defer cow.buf.Reset()
+
 	var attribute string
 	if cow.bold {
 		attribute = ";1"
 	}
 
-	buf := make([]rune, 0, len(mow))
 	for _, char := range mow {
 		if char == '\n' {
-			buf = append(buf, char)
+			cow.buf.WriteRune(char)
 			continue
 		}
-		buf = append(buf, []rune(fmt.Sprintf("\033[38;5;%d%sm%c\033[0m", rgb(float64(i)), attribute, char))...)
+		fmt.Fprintf(&cow.buf, "\033[38;5;%d%sm%c\033[0m", rgb(float64(i)), attribute, char)
 		i++
 	}
-	return string(buf)
+	return cow.buf.String()
 }
 
 const (
@@ -62,9 +64,9 @@ const (
 	m    = math.Pi / 3
 )
 
-func rgb(i float64) int {
-	red := int(6*((math.Sin(freq*i+0)*127+128)/256)) * 36
-	green := int(6*((math.Sin(freq*i+2*m)*127+128)/256)) * 6
-	blue := int(6*((math.Sin(freq*i+4*m)*127+128)/256)) * 1
+func rgb(i float64) int64 {
+	red := int64(6*((math.Sin(freq*i+0)*127+128)/256)) * 36
+	green := int64(6*((math.Sin(freq*i+2*m)*127+128)/256)) * 6
+	blue := int64(6*((math.Sin(freq*i+4*m)*127+128)/256)) * 1
 	return 16 + red + green + blue
 }
